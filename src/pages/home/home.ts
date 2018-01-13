5121c809afd546546f5ab6c23bfe2b17b187ba19
Dennis Müller <dennis.mueller@eatclever.de>
2018-01-13 14:29:52 +0100

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController) {
 
